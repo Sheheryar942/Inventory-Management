0ca8cde3d92c6e462be30406c7688f22c250039a
Sheheryar942 <132945798+Sheheryar942@users.noreply.github.com>
2023-05-09 00:47:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
 # Import libraries
 import pandas as pd
 import numpy as np
-from statsmodels.tsa.arima.model import ARIMA
-from scipy.optimize import minimize
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
-# Load data
-data = pd.read_csv('inventory_data.csv')
+# Create random data for inventory management
+n_samples = 1000
+n_products = 5
 
-# Preprocess data
-...
+# Generate random sales data
+sales_data = np.random.randint(50, 100, size=(n_samples, n_products))
 
-# Fit ARIMA model
-model = ARIMA(data, order=(1,1,1))
-model_fit = model.fit()
+# Generate random inventory data
+inventory_data = np.random.randint(10, 30, size=(n_samples, n_products))
 
-# Forecast future demand
-forecast = model_fit.forecast(steps=30)
+# Generate random price data
+price_data = np.random.uniform(1, 10, size=n_products)
 
-# Optimize inventory levels
-def objective(x):
-    return -np.sum(np.multiply(forecast, x))
+# Generate random promotion data
+promotion_data = np.random.randint(0, 2, size=(n_samples, n_products))
 
-constraints = [{'type': 'ineq', 'fun': lambda x: x[0] + x[1] - 50},
-               {'type': 'ineq', 'fun': lambda x: x[2] - 20},
-               {'type': 'ineq', 'fun': lambda x: x[3] - 30}]
-x0 = np.array([10, 20, 10, 20])
-result = minimize(objective, x0, method='SLSQP', constraints=constraints)
+# Generate random demand data
+demand_data = np.sum(sales_data, axis=1)
 
-# Print optimal inventory levels
-print('Optimal inventory levels:')
-print(result.x)
+# Combine data into DataFrame
+data = pd.DataFrame(sales_data, columns=[f'Product {i+1} Sales' for i in range(n_products)])
+data = data.join(pd.DataFrame(inventory_data, columns=[f'Product {i+1} Inventory' for i in range(n_products)]))
+data['Price'] = price_data
+data = data.join(pd.DataFrame(promotion_data, columns=[f'Product {i+1} Promotion' for i in range(n_products)]))
+data['Demand'] = demand_data
+
+# Split data into training and testing sets
+X = data.drop('Demand', axis=1)
+y = data['Demand']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Train a linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Evaluate the model
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print('Mean squared error:', mse)
+print('R^2 score:', r2)
